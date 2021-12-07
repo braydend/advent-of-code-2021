ba1eb7b1399433224d7feb7f4b1ee523a5f2aa5b
Brayden <braydendiluca@gmail.com>
2021-12-07 20:38:13 +1100

    Day one part 2

--- day-1/main.go
+++ day-1/main.go
@@ -9,14 +9,26 @@ import (
 )
 
 func main() {
-	depths, err := parseDepthsFromFile("input.txt")
-
+	depthReport, err := ReportDepths("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(depthReport)
+}
 
-	fmt.Println(depthsToString(depths))
-	fmt.Printf("%d depth increases\n", countDepthIncreases(depths))
+func ReportDepths(filename string) (output string, err error) {
+	depthsFromFile, err := parseDepthsFromFile(filename)
+
+	if err != nil {
+		return "", err
+	}
+	windows := createDepthWindows(depthsFromFile)
+	depths := sumDepthWindows(windows)
+
+	output = fmt.Sprintf("%s%s\n", output, depthsToString(depths))
+	output = fmt.Sprintf("%s%d depth increases\n", output, countDepthIncreases(depths))
+
+	return output, nil
 }
 
 func parseDepthsFromFile(filename string) (depths []int, err error) {
@@ -27,6 +39,30 @@ func parseDepthsFromFile(filename string) (depths []int, err error) {
 	}
 
 	return parseDepths(string(data))
+}
+
+func createDepthWindows(depths []int) (windows [][3]int) {
+	windowCount := len(depths) - 2
+	windows = make([][3]int, windowCount)
+
+	for i := 0; i < windowCount; i++ {
+		for j := range depths {
+			if j < 3 {
+				windows[i][j] = depths[i+j]
+			}
+		}
+
+	}
+
+	return windows
+}
+
+func sumDepthWindows(windows [][3]int) (sums []int) {
+	for _, window := range windows {
+		sums = append(sums, window[0]+window[1]+window[2])
+	}
+
+	return sums
 }
 
 func parseDepths(input string) (depths []int, err error) {
@@ -53,18 +89,35 @@ func parseDepths(input string) (depths []int, err error) {
 	return depths, nil
 }
 
+func getAsciiLetterForIndex(index int) (output string) {
+	for i := index; i >= 0; i = i - 26 {
+		//if i > 26 {
+		//	letter := rune(int(math.Floor(float64(i/26))) + 65)
+		//	output = fmt.Sprintf("%s%c", output, letter)
+		//} else {
+		letter := rune(int(i%26) + 65)
+		output = fmt.Sprintf("%s%c", output, letter)
+		//}
+	}
+
+	return output
+}
+
 func depthsToString(depths []int) (output string) {
 	for i, depth := range depths {
 		var message string
+
 		if i == 0 {
-			message = "N/A - no previous measurement"
+			message = "N/A - no previous sum"
+		} else if depth == depths[i-1] {
+			message = "no change"
 		} else if depth > depths[i-1] {
 			message = "increased"
 		} else {
 			message = "decreased"
 		}
 
-		output = fmt.Sprintf("%s%d (%s)\n", output, depth, message)
+		output = fmt.Sprintf("%s%s: %d (%s)\n", output, getAsciiLetterForIndex(i), depth, message)
 	}
 
 	return output
