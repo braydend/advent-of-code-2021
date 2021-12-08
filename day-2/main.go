6b2d49c0bfe2d597ba5428df12789c9adce4541f
Bray <braydendiluca@gmail.com>
2021-12-08 18:29:03 +1100

    Day 2 part 1

---
+++ day-2/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Direction int
+
+const (
+	Up Direction = iota
+	Down
+	Forward
+)
+
+type Position struct {
+	horizontal uint
+	depth      uint
+}
+
+type Instruction struct {
+	direction Direction
+	amount    uint
+}
+
+func main() {
+	instructions, err := parseDirectionsFromFile("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	position := Navigate(instructions)
+
+	fmt.Printf("Sub is at %d depth and %d horizontal", position.depth, position.horizontal)
+}
+
+func parseDirectionsFromFile(filename string) (instructions []Instruction, err error) {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, line := range strings.Split(string(data), "\n") {
+		if line == "" {
+			continue
+		}
+
+		splits := strings.Split(line, " ")
+		var direction Direction
+		directionString := splits[0]
+		amount, err := strconv.Atoi(splits[1])
+
+		if err != nil {
+			return nil, err
+		}
+
+		switch directionString {
+		case "up":
+			direction = Up
+			break
+
+		case "down":
+			direction = Down
+			break
+
+		case "forward":
+			direction = Forward
+			break
+
+		default:
+			return nil, fmt.Errorf("Unknown direction: %s", directionString)
+		}
+
+		instructions = append(instructions, Instruction{direction, uint(amount)})
+	}
+
+	return instructions, nil
+}
+
+func Navigate(instructions []Instruction) (position Position) {
+	for _, instruction := range instructions {
+		switch instruction.direction {
+		case Up:
+			position = Position{position.horizontal, position.depth - instruction.amount}
+			break
+
+		case Down:
+			position = Position{position.horizontal, position.depth + instruction.amount}
+			break
+
+		case Forward:
+			position = Position{position.horizontal + instruction.amount, position.depth}
+			break
+		}
+	}
+
+	return position
+}
---
+++ day-2/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNavigate(t *testing.T) {
+	type args struct {
+		instructions []Instruction
+	}
+	tests := []struct {
+		name string
+		args args
+		want Position
+	}{
+		{"Correctly navigates", args{instructions: []Instruction{{Forward, 5}, {Down, 5}, {Forward, 8}, {Up, 3}, {Down, 8}, {Forward, 2}}}, Position{15, 10}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Navigate(tt.args.instructions); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Navigate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
