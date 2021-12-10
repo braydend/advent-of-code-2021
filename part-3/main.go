635810f29d073a905841cf8876846315adbf22ed
Brayden <braydendiluca@gmail.com>
2021-12-11 00:35:24 +1100

    Add gamma rate calculation with test

--- part-3/main.go
+++ part-3/main.go
@@ -1,96 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"math"
-	"os"
-	"strings"
-)
-
-const AsciiOne = 49
+import "math"
 
 func main() {
-	readings, err := parseReadingsFromFile("input.txt")
 
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	gamma := CalculateGammaRate(readings)
-	epsilon := CalculateEpsilonRate(readings)
-
-	fmt.Printf("gamma rate: %d\nepsilon rate: %d\n", gamma, epsilon)
-	fmt.Printf("Total power consumption is: %d\n", gamma*epsilon)
 }
 
-func parseReadingsFromFile(filename string) (output [][]bool, err error) {
-	data, err := os.ReadFile(filename)
+func CalculateGammaRate(input [][5]bool) (result uint) {
+	var bits [5]bool
 
-	if err != nil {
-		return nil, err
-	}
-
-	lines := strings.Split(string(data), "\n")
-
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-
-		var reading []bool
-		//output := append(output, []bool{})
-		for _, char := range line {
-			//output := append(output, []bool{})
-			reading = append(reading, char == AsciiOne)
-		}
-		output = append(output, reading)
-	}
-
-	return output, nil
-}
-
-func CalculateGammaRate(input [][]bool) uint {
-	return binaryBoolSliceToUint(getMajorityBits(input))
-}
-
-func CalculateEpsilonRate(input [][]bool) uint {
-	return binaryBoolSliceToUint(invertBinarySlice(getMajorityBits(input)))
-}
-
-func getMajorityBits(readings [][]bool) (output []bool) {
-
-	for i := 0; i < len(readings[0]); i++ {
+	for i := 0; i < 4; i++ {
 		var trueCount uint
+		var falseCount uint
 
-		for _, reading := range readings {
+		for _, reading := range input {
 			if reading[i] {
 				trueCount++
 				continue
 			}
+			falseCount++
 		}
 
-		output = append(output, trueCount > uint(len(readings)/2))
+		if trueCount > falseCount {
+			bits[i] = true
+			continue
+		}
+		bits[i] = false
 	}
 
-	return output
-}
-
-func binaryBoolSliceToUint(slice []bool) (output uint) {
-	for i, bit := range slice {
-		exponent := len(slice) - 1 - i
+	for i, bit := range bits {
 		if bit {
-			output += uint(math.Pow(2, float64(exponent)))
+			result += uint(math.Pow(2, float64(len(bits)-1-i)))
 		}
 	}
 
-	return output
-}
-
-func invertBinarySlice(input []bool) (output []bool) {
-	for _, value := range input {
-		output = append(output, !value)
-	}
-
-	return output
+	return result
 }
--- part-3/main_test.go
+++ part-3/main_test.go
@@ -2,56 +2,34 @@ package main
 
 import "testing"
 
-var testInput = [][]bool{
-	{false, false, true, false, false},
-	{true, true, true, true, false},
-	{true, false, true, true, false},
-	{true, false, true, true, true},
-	{true, false, true, false, true},
-	{false, true, true, true, true},
-	{false, false, true, true, true},
-	{true, true, true, false, false},
-	{true, false, false, false, false},
-	{true, true, false, false, true},
-	{false, false, false, true, false},
-	{false, true, false, true, false},
-}
-
 func TestCalculateGammaRate(t *testing.T) {
 	type args struct {
-		input [][]bool
+		input [][5]bool
 	}
 	tests := []struct {
 		name       string
 		args       args
 		wantResult uint
 	}{
-		{"Calculate gamma rate correctly", args{testInput}, 22},
+		{"Calculate gamma rate correctly", args{[][5]bool{
+			{false, false, true, false, false},
+			{true, true, true, true, false},
+			{true, false, true, true, false},
+			{true, false, true, true, true},
+			{true, false, true, false, true},
+			{false, true, true, true, true},
+			{false, false, true, true, true},
+			{true, true, true, false, false},
+			{true, false, false, false, false},
+			{true, true, false, false, true},
+			{false, false, false, true, false},
+			{false, true, false, true, false},
+		}}, 22},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotResult := CalculateGammaRate(tt.args.input); gotResult != tt.wantResult {
 				t.Errorf("CalculateGammaRate() = %v, want %v", gotResult, tt.wantResult)
-			}
-		})
-	}
-}
-
-func TestCalculateEpsilonRate(t *testing.T) {
-	type args struct {
-		input [][]bool
-	}
-	tests := []struct {
-		name       string
-		args       args
-		wantResult uint
-	}{
-		{"Calculates sigma correctly", args{testInput}, 9},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if gotResult := CalculateEpsilonRate(tt.args.input); gotResult != tt.wantResult {
-				t.Errorf("CalculateEpsilonRate() = %v, want %v", gotResult, tt.wantResult)
 			}
 		})
 	}
