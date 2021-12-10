6855347e72045dd177466dbe303e28d7631430c1
Brayden <braydendiluca@gmail.com>
2021-12-11 01:03:09 +1100

    Add epsilon rate calculation with test

--- part-3/main.go
+++ part-3/main.go
@@ -11,7 +11,7 @@ func main() {
 func CalculateGammaRate(input [][ReadingLength]bool) (result uint) {
 	var majorityBits [ReadingLength]bool
 
-	for i := 0; i < ReadingLength-1; i++ {
+	for i := 0; i < ReadingLength; i++ {
 		var trueCount uint
 
 		for _, reading := range input {
@@ -26,6 +26,32 @@ func CalculateGammaRate(input [][ReadingLength]bool) (result uint) {
 
 	for i, bit := range majorityBits {
 		exponent := len(majorityBits) - 1 - i
+		if bit {
+			result += uint(math.Pow(2, float64(exponent)))
+		}
+	}
+
+	return result
+}
+
+func CalculateEpsilonRate(input [][ReadingLength]bool) (result uint) {
+	var minorityBits [ReadingLength]bool
+
+	for i := 0; i < ReadingLength; i++ {
+		var trueCount uint
+
+		for _, reading := range input {
+			if reading[i] {
+				trueCount++
+				continue
+			}
+		}
+
+		minorityBits[i] = trueCount < uint(len(input)/2)
+	}
+
+	for i, bit := range minorityBits {
+		exponent := len(minorityBits) - 1 - i
 		if bit {
 			result += uint(math.Pow(2, float64(exponent)))
 		}
