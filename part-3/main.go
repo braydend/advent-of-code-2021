c0d3df9c619a9abd25bf26313f0d372639e4f7d6
Brayden <braydendiluca@gmail.com>
2021-12-11 00:43:22 +1100

    Refactor gamma rate calculation

--- part-3/main.go
+++ part-3/main.go
@@ -2,35 +2,32 @@ package main
 
 import "math"
 
+const ReadingLength = 5
+
 func main() {
 
 }
 
-func CalculateGammaRate(input [][5]bool) (result uint) {
-	var bits [5]bool
+func CalculateGammaRate(input [][ReadingLength]bool) (result uint) {
+	var majorityBits [ReadingLength]bool
 
-	for i := 0; i < 4; i++ {
+	for i := 0; i < ReadingLength-1; i++ {
 		var trueCount uint
-		var falseCount uint
 
 		for _, reading := range input {
 			if reading[i] {
 				trueCount++
 				continue
 			}
-			falseCount++
 		}
 
-		if trueCount > falseCount {
-			bits[i] = true
-			continue
-		}
-		bits[i] = false
+		majorityBits[i] = trueCount > uint(len(input)/2)
 	}
 
-	for i, bit := range bits {
+	for i, bit := range majorityBits {
+		exponent := len(majorityBits) - 1 - i
 		if bit {
-			result += uint(math.Pow(2, float64(len(bits)-1-i)))
+			result += uint(math.Pow(2, float64(exponent)))
 		}
 	}
 
