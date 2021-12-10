78da9afc9735179a2721d3211d812bde794c924c
Brayden <braydendiluca@gmail.com>
2021-12-11 01:18:02 +1100

    Refactor epsilon calculation

--- part-3/main.go
+++ part-3/main.go
@@ -8,54 +8,47 @@ func main() {
 
 }
 
-func CalculateGammaRate(input [][ReadingLength]bool) (result uint) {
-	var majorityBits [ReadingLength]bool
-
-	for i := 0; i < ReadingLength; i++ {
-		var trueCount uint
-
-		for _, reading := range input {
-			if reading[i] {
-				trueCount++
-				continue
-			}
-		}
-
-		majorityBits[i] = trueCount > uint(len(input)/2)
-	}
-
-	for i, bit := range majorityBits {
-		exponent := len(majorityBits) - 1 - i
-		if bit {
-			result += uint(math.Pow(2, float64(exponent)))
-		}
-	}
-
-	return result
+func CalculateGammaRate(input [][ReadingLength]bool) uint {
+	return binaryBoolSliceToUint(getMajorityBits(input))
 }
 
-func CalculateEpsilonRate(input [][ReadingLength]bool) (result uint) {
-	var minorityBits [ReadingLength]bool
+func CalculateEpsilonRate(input [][ReadingLength]bool) uint {
+	return binaryBoolSliceToUint(invertBinarySlice(getMajorityBits(input)))
+}
+
+func getMajorityBits(readings [][ReadingLength]bool) (output [ReadingLength]bool) {
 
 	for i := 0; i < ReadingLength; i++ {
 		var trueCount uint
 
-		for _, reading := range input {
+		for _, reading := range readings {
 			if reading[i] {
 				trueCount++
 				continue
 			}
 		}
 
-		minorityBits[i] = trueCount < uint(len(input)/2)
+		output[i] = trueCount > uint(len(readings)/2)
 	}
 
-	for i, bit := range minorityBits {
-		exponent := len(minorityBits) - 1 - i
+	return output
+}
+
+func binaryBoolSliceToUint(slice [ReadingLength]bool) (output uint) {
+	for i, bit := range slice {
+		exponent := len(slice) - 1 - i
 		if bit {
-			result += uint(math.Pow(2, float64(exponent)))
+			output += uint(math.Pow(2, float64(exponent)))
 		}
 	}
 
-	return result
+	return output
+}
+
+func invertBinarySlice(input [ReadingLength]bool) (output [ReadingLength]bool) {
+	for i, value := range input {
+		output[i] = !value
+	}
+
+	return output
 }
