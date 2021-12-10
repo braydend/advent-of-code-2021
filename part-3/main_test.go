6b558d632baa4c1e45c20f783478f6b9b9bda028
Brayden <braydendiluca@gmail.com>
2021-12-11 01:03:09 +1100

    Add epsilon rate calculation with test

--- part-3/main_test.go
+++ part-3/main_test.go
@@ -2,34 +2,56 @@ package main
 
 import "testing"
 
+var testInput = [][5]bool{
+	{false, false, true, false, false},
+	{true, true, true, true, false},
+	{true, false, true, true, false},
+	{true, false, true, true, true},
+	{true, false, true, false, true},
+	{false, true, true, true, true},
+	{false, false, true, true, true},
+	{true, true, true, false, false},
+	{true, false, false, false, false},
+	{true, true, false, false, true},
+	{false, false, false, true, false},
+	{false, true, false, true, false},
+}
+
 func TestCalculateGammaRate(t *testing.T) {
 	type args struct {
-		input [][5]bool
+		input [][ReadingLength]bool
 	}
 	tests := []struct {
 		name       string
 		args       args
 		wantResult uint
 	}{
-		{"Calculate gamma rate correctly", args{[][5]bool{
-			{false, false, true, false, false},
-			{true, true, true, true, false},
-			{true, false, true, true, false},
-			{true, false, true, true, true},
-			{true, false, true, false, true},
-			{false, true, true, true, true},
-			{false, false, true, true, true},
-			{true, true, true, false, false},
-			{true, false, false, false, false},
-			{true, true, false, false, true},
-			{false, false, false, true, false},
-			{false, true, false, true, false},
-		}}, 22},
+		{"Calculate gamma rate correctly", args{testInput}, 22},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotResult := CalculateGammaRate(tt.args.input); gotResult != tt.wantResult {
 				t.Errorf("CalculateGammaRate() = %v, want %v", gotResult, tt.wantResult)
+			}
+		})
+	}
+}
+
+func TestCalculateEpsilonRate(t *testing.T) {
+	type args struct {
+		input [][ReadingLength]bool
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult uint
+	}{
+		{"Calculates sigma correctly", args{testInput}, 9},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotResult := CalculateEpsilonRate(tt.args.input); gotResult != tt.wantResult {
+				t.Errorf("CalculateEpsilonRate() = %v, want %v", gotResult, tt.wantResult)
 			}
 		})
 	}
