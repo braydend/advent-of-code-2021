1490d92063c30f22e4e32857e18a5b8de7851814
Brayden <braydendiluca@gmail.com>
2021-12-13 20:11:20 +1100

    Create method to check if a board is complete

---
+++ day-4/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+var (
+	testBoard   = Bingo{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 0}, {10, 11, 12, 13, 14}, {15, 16, 17, 18, 19}, {20, 21, 22, 23, 24}}
+	completeRow = []int{1, 2, 3, 4, 5}
+	completeCol = []int{1, 6, 10, 15, 20}
+)
+
+func TestCheckBoard(t *testing.T) {
+	type args struct {
+		board   Bingo
+		numbers []int
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantIsComplete bool
+	}{
+		{"Board is not complete", args{testBoard, []int{1, 7, 3, 24, 5}}, false},
+		{"Board has complete row", args{testBoard, completeRow}, true},
+		{"Board has complete col", args{testBoard, completeCol}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotIsComplete := CheckBoard(tt.args.board, tt.args.numbers); gotIsComplete != tt.wantIsComplete {
+				t.Errorf("CheckBoard() = %v, want %v", gotIsComplete, tt.wantIsComplete)
+			}
+		})
+	}
+}
