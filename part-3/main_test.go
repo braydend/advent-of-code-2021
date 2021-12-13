c03dee4374a97836b6e0596f6e728170770d95d8
Brayden <braydendiluca@gmail.com>
2021-12-13 16:05:19 +1100

    Calculate CO2 scrubber rating

--- part-3/main_test.go
+++ part-3/main_test.go
@@ -76,3 +76,23 @@ func TestCalculateOxygenGeneratorRating(t *testing.T) {
 		})
 	}
 }
+
+func TestCalculateCo2ScrubberRating(t *testing.T) {
+	type args struct {
+		input [][]bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint
+	}{
+		{"Calculates CO2 scrubber rating correctly", args{testInput}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalculateCo2ScrubberRating(tt.args.input); got != tt.want {
+				t.Errorf("CalculateCo2ScrubberRating() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
