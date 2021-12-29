b230e447ead83c95615da5d97eb7a41e4cdb982d
Bray <braydendiluca@gmail.com>
2021-12-29 14:57:25 +1000

    refactor GetCoveredCoordinates

--- day-5/vent/vent.go
+++ day-5/vent/vent.go
@@ -14,27 +14,44 @@ func NewVent(x1, y1, x2, y2 int) Vent {
 	return Vent{position{x1, y1}, position{x2, y2}}
 }
 
-func (v Vent) GetCoveredCoordinates() (coveredPositions []position) {
-	var highestX, highestY, lowestX, lowestY int
-
+func (v Vent) findHighestXAndY() (highest position) {
 	if v.startingPosition.x >= v.endingPosition.x {
-		highestX = v.startingPosition.x
-		lowestX = v.endingPosition.x
+		highest.x = v.startingPosition.x
 	} else {
-		highestX = v.endingPosition.x
-		lowestX = v.startingPosition.x
+		highest.x = v.endingPosition.x
 	}
 
 	if v.startingPosition.y >= v.endingPosition.y {
-		highestY = v.startingPosition.y
-		lowestY = v.endingPosition.y
+		highest.y = v.startingPosition.y
 	} else {
-		highestY = v.endingPosition.y
-		lowestY = v.startingPosition.y
+		highest.y = v.endingPosition.y
 	}
 
-	for x := lowestX; x <= highestX; x++ {
-		for y := lowestY; y <= highestY; y++ {
+	return highest
+}
+
+func (v Vent) findLowestXAndY() (lowest position) {
+	if v.startingPosition.x >= v.endingPosition.x {
+		lowest.x = v.endingPosition.x
+	} else {
+		lowest.x = v.startingPosition.x
+	}
+
+	if v.startingPosition.y >= v.endingPosition.y {
+		lowest.y = v.endingPosition.y
+	} else {
+		lowest.y = v.startingPosition.y
+	}
+
+	return lowest
+}
+
+func (v Vent) GetCoveredCoordinates() (coveredPositions []position) {
+	highest := v.findHighestXAndY()
+	lowest := v.findLowestXAndY()
+
+	for x := lowest.x; x <= highest.x; x++ {
+		for y := lowest.y; y <= highest.y; y++ {
 			coveredPositions = append(coveredPositions, position{x, y})
 		}
 	}
