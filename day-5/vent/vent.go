7d0470d81ddb696a1a1ab236a1f8f19c1b05a607
Bray <braydendiluca@gmail.com>
2021-12-29 14:35:56 +1000

    Add GetCoveredCoordinates method to Vent struct

--- day-5/vent/vent.go
+++ day-5/vent/vent.go
@@ -14,6 +14,34 @@ func NewVent(x1, y1, x2, y2 int) Vent {
 	return Vent{position{x1, y1}, position{x2, y2}}
 }
 
+func (v Vent) GetCoveredCoordinates() (coveredPositions []position) {
+	var highestX, highestY, lowestX, lowestY int
+
+	if v.startingPosition.x >= v.endingPosition.x {
+		highestX = v.startingPosition.x
+		lowestX = v.endingPosition.x
+	} else {
+		highestX = v.endingPosition.x
+		lowestX = v.startingPosition.x
+	}
+
+	if v.startingPosition.y >= v.endingPosition.y {
+		highestY = v.startingPosition.y
+		lowestY = v.endingPosition.y
+	} else {
+		highestY = v.endingPosition.y
+		lowestY = v.startingPosition.y
+	}
+
+	for x := lowestX; x <= highestX; x++ {
+		for y := lowestY; y <= highestY; y++ {
+			coveredPositions = append(coveredPositions, position{x, y})
+		}
+	}
+
+	return coveredPositions
+}
+
 func (v Vent) IsHorizontal() bool {
 	return v.startingPosition.x == v.endingPosition.x || v.startingPosition.y == v.endingPosition.y
 }
--- day-5/vent/vent_test.go
+++ day-5/vent/vent_test.go
@@ -1,8 +1,35 @@
 package vent
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestVent_GetCoveredCoordinates(t *testing.T) {
+	type fields struct {
+		startingPosition position
+		endingPosition   position
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []position
+	}{
+		{"Correctly finds covered coordinates across same x-axis", fields{position{1, 1}, position{1, 3}}, []position{{1, 1}, {1, 2}, {1, 3}}},
+		{"Correctly finds covered coordinates across same y-axis", fields{position{9, 7}, position{7, 7}}, []position{{7, 7}, {8, 7}, {9, 7}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := Vent{
+				startingPosition: tt.fields.startingPosition,
+				endingPosition:   tt.fields.endingPosition,
+			}
+			if got := v.GetCoveredCoordinates(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetCoveredCoordinates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestVent_IsHorizontal(t *testing.T) {
 	type fields struct {
